ba5089228a3025ee1127e7278ae7ba5255e4b3da
saksham <sakshamchauhan79@gmail.com>
2022-09-15 11:31:16 +0530

    removed unused code

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,6 @@ import React from "react";
 import AnimatedCursor from "react-animated-cursor";
 import "./App.scss";
 import Header from "./components/Header/Header";
-
-//import Blog2 from './pages/BlogPages/Blog_2'
-//import Blog1 from './pages/BlogPages/Blog1'
 import Main from "./pages/Main";
 
 function App() {

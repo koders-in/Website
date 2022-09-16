2207fa53b9e9d275a225ce12a216580b8b88bd99
Jack1998 <yaduvanshiabhi1998@gmail.com>
2022-09-16 11:32:01 +0530

    add cookie consent functionality

--- src/App.jsx
+++ src/App.jsx
@@ -2,9 +2,7 @@ import React from "react";
 import AnimatedCursor from "react-animated-cursor";
 import "./App.scss";
 import Header from "./components/Header/Header";
-
-//import Blog2 from './pages/BlogPages/Blog_2'
-//import Blog1 from './pages/BlogPages/Blog1'
+import CookieConsent from "react-cookie-consent";
 import Main from "./pages/Main";
 
 function App() {
@@ -20,6 +18,35 @@ function App() {
       />
       <Header />
       <Main />
+      <CookieConsent
+        buttonText="Accept All"
+        enableDeclineButton
+        cookieName="koders-website"
+        setDeclineCookie={false}
+        declineButtonText="Decline"
+        onAccept={() => {}}
+        style={{
+          background: "#1d2c48",
+          fontSize: "14px",
+          textAlign: "left",
+        }}
+        buttonStyle={{
+          padding: "10px 20px",
+          border: "1px solid #ffffff",
+          color: "#ffffff",
+          background: "transparent",
+        }}
+        declineButtonStyle={{
+          border: "1px solid gray",
+          background: "transparent",
+          padding: "10px 20px",
+          color: "gray",
+        }}
+      >
+        We use cookies to enhance your browsing experience, serve personalized
+        ads or content, and analyze our traffic. By clicking 'Accept All', you
+        consent to our use of cookies.
+      </CookieConsent>
     </div>
   );
 }

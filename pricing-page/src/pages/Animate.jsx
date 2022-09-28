f3a2de48871ad2914e0355d4f1ecdd1aef97025e
saksham <sakshamchauhan79@gmail.com>
2022-09-28 12:32:29 +0530

    remove unused code

--- pricing-page/src/pages/Animate.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./style.css";
-
-export const Animate = ({ text, className }) => {
-  console.log("anim", text);
-  return <h1 className={className}>{text}</h1>;
-};

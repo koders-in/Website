f21289453d6f020b7214f5c17801be1ec2daa6a8
Saksham <saksham@Sakshams-MacBook-Air.local>
2023-01-03 19:46:22 +0530

    feat: add auth in hasura apis

--- helper/api.ts
+++ helper/api.ts
@@ -3,7 +3,8 @@ const axios = require("axios");
 export const JobClient = axios.create({
   baseURL: "https://hasura.koders.in/api/rest/",
   headers:{
-    'Content-type':'application/json'
+    'Content-type':'application/json',
+    'x-hasura-admin-secret': "api-key"
   }
 });
  

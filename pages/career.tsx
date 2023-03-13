d49136e9eeacd7b8911579d04e8d1746a0556fd3
Saksham <saksham@Sakshams-MacBook-Air.local>
2023-03-13 17:27:21 +0530

    chore:optimize data

--- pages/career.tsx
+++ pages/career.tsx
@@ -43,61 +43,6 @@ const Navbar = dynamic(() => import("../components/Navbar"), {
 const Jobs = () => {
   const { jobs, pinJobs, noMatch, handleTryAgain, viewMore, handleViewMore } =
     useCareerHook();
-  // const  [filter, setFilter] = useState({
-  //   title: "",
-  //   isRemote: false,
-  //   departments: ["All"],
-  // });
-  // const [jobs, setJobs] = useState<any>(null);
-  // const [viewMore, setViewMore] = useState<boolean>(true);
-  // const [pinJobs, setPinJobs] = useState<any>();
-  // const [tempData, setTempData] = useState<any>(null);
-  // const [noMatch, setNoMatch] = useState<boolean>(false);
-  // const [department, setDepartment] = useState<Array<string>>(["All"]);
-  // const fetchData = useFetchDataFromServer();
-  // const filterData = useFilter();
-
-  // useEffect(() => {
-  //   AOS.init({
-  //     easing: "ease-out",
-  //     once: true,
-  //     duration: 600,
-  //   });
-  // }, []);
-
-  // useEffect(() => {
-  //   if (jobs === null || jobs === undefined)
-  //     fetchData("open-job-listings", setJobs);
-
-  //   if (jobs && tempData === null) {
-  //     if (jobs?.jobs_listing?.length / 3 > 0) {
-  //       setPinJobs(jobs?.jobs_listing?.slice(0, 2));
-  //       setTempData(jobs?.jobs_listing?.slice(0, 2));
-  //     } else {
-  //       setPinJobs(jobs?.jobs_listing);
-  //       setTempData(jobs?.jobs_listing);
-  //     }
-  //   }
-  // }, [jobs, pinJobs]);
-
-  // const handleTryAgain = async () => {
-  //   setPinJobs(false);
-  //   let res: any = await fetchData("open-job-listings", setJobs);
-  //   if (res?.jobs_listing?.length > 3) {
-  //     setPinJobs(res?.jobs_listing?.slice(0, 3));
-  //   } else {
-  //     setPinJobs(res?.jobs_listing);
-  //   }
-  // };
-
-  // const handleViewMore = () => {
-  //   if (!jobs?.jobs_listing?.length) return;
-  //   const tempArr = [...jobs?.jobs_listing];
-  //   setPinJobs(tempArr);
-  //   setTempData(tempArr);
-  //   setViewMore(false);
-  //   filterData(filter, tempArr, setPinJobs);
-  // };
 
   return (
     <div className="bg-main-primary overflow-hidden relative">
@@ -112,18 +57,7 @@ const Jobs = () => {
           <Divider className="mt-9" />
         </Suspense>
         <Suspense fallback={<div className="text-main-teal">Loading...</div>}>
-          {/* <LandingSection
-            {...{
-              pinJobs,
-              setPinJobs,
-              setNoMatch,
-              tempData,
-              department,
-              setDepartment,
-              filter,
-              setFilter,
-            }}
-          /> */}
+          <LandingSection />
         </Suspense>
         <Suspense fallback={<div className="text-main-teal">Loading...</div>}>
           <Divider className="mt-12" />
